afdfbded38717ca30da1c781988f4a4882204e4f
edrikfsteiner <edrikfsteiner@gmail.com>
2024-10-23 18:34:24 -0300

    Update Login Layout

---
+++ src/app/components/login-layout/login-layout.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-layout.component.html',
+  styleUrl: './login-layout.component.scss'
+})
+export class LoginLayoutComponent {
+  @Input() title: string = "";
+  @Input() primaryBtnText: string = "";
+  @Input() secondaryBtnText: string = "";
+  @Input() disablePrimaryBtnText: boolean = true;
+  @Output("submit") onSubmit =  new EventEmitter();
+  @Output("navigate") onNavigate = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
+
+  navigate(){
+    this.onNavigate.emit();
+  }
+}
